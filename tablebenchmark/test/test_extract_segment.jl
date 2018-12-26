312ff2b6fe398abc06cc940755bbc579053ba68d
U-A-IHPC\zhangly <zhangly@IHP-SL18-60055.A-IHPC.shared-svc.local>
2018-12-26 08:14:34 +0800

    Add Julia version without precompile

--- tablebenchmark/test/test_extract_segment.jl
+++ tablebenchmark/test/test_extract_segment.jl
@@ -1,29 +1,31 @@
 using JuliaDB
 using Test
 include("../src/extract_segment.jl")
-#
-# @testset "Julia Version RunningTime without precompile" begin
-#     gtfs_dir = "../data/google_transit_manhattan"
-#
-#     @info "Check the time of loading CSV files"
-#     dtype=Dict("shape_id"=>String)
-#     @info "Open `shapes.txt`"
-#     @time frm_shape = loadtable(joinpath(gtfs_dir, "shapes.txt"), colparsers=dtype)
-#     @info "Open `stop_times.txt`"
-#     @time frm_stop_time = loadtable( joinpath(gtfs_dir, "stop_times.txt"), colparsers=Dict("stop_id"=>String, "shape_id"=>String) )
-#     @info "Open `trips.txt`"
-#     @time frm_trip = loadtable( joinpath(gtfs_dir, "trips.txt"), colparsers=Dict("trip_id"=>String, "shape_id"=>String) )
-#     @info "Open `stops.txt`"
-#     @time frm_stop = loadtable( joinpath(gtfs_dir, "stops.txt"), colparsers=Dict("stop_id"=>String) )
-#
-#     segements = Dict{Tuple{String, String},Vector{Tuple{Float64, Float64}}}()
-#     @info "The running time of processing 500 rows:"
-#     @time extract_segment(segements, frm_trip, frm_stop, frm_stop_time, frm_shape)
-# end
 
-@testset "Julia Version RunningTime use Benchmark" begin
+@testset "Julia Version RunningTime without precompile" begin
     gtfs_dir = "../data/google_transit_manhattan"
 
+    @info "---------------Julia without precompile"
+    @info "Check the time of loading CSV files"
+    dtype=Dict("shape_id"=>String)
+    @info "Open `shapes.txt`"
+    @time frm_shape = loadtable(joinpath(gtfs_dir, "shapes.txt"), colparsers=dtype)
+    @info "Open `stop_times.txt`"
+    @time frm_stop_time = loadtable( joinpath(gtfs_dir, "stop_times.txt"), colparsers=Dict("stop_id"=>String, "shape_id"=>String) )
+    @info "Open `trips.txt`"
+    @time frm_trip = loadtable( joinpath(gtfs_dir, "trips.txt"), colparsers=Dict("trip_id"=>String, "shape_id"=>String) )
+    @info "Open `stops.txt`"
+    @time frm_stop = loadtable( joinpath(gtfs_dir, "stops.txt"), colparsers=Dict("stop_id"=>String) )
+
+    segements = Dict{Tuple{String, String},Vector{Tuple{Float64, Float64}}}()
+    @info "The running time of processing 500 rows:"
+    @time extract_segment(segements, frm_trip, frm_stop, frm_stop_time, frm_shape)
+end
+
+@testset "Julia Version RunningTime (precompile)" begin
+    gtfs_dir = "../data/google_transit_manhattan"
+
+    @info "---------------Julia with precompile"
     @info "Check the time of loading CSV files"
     dtype=Dict("shape_id"=>String)
     @info "Open `shapes.txt`"
